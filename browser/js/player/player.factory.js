cb18b87315f57d7c58f87ffb09733d38a9659387
Peter Lynn <github@fantutte.net>
2016-07-06 16:07:04 -0400

    checkpoint

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,5 +1,51 @@
 'use strict';
 
-juke.factory('PlayerFactory', function(){
-  // non-UI logic in here
+juke.factory('PlayerFactory', function($http){
+  var returnObj = {};
+  var audio = document.createElement('audio');
+  var currentlyPlaying = false;
+
+  returnObj.start = function(song) {
+    returnObj.pause();
+    // enable loading new song
+    audio.src = song.audioUrl;
+    audio.load();
+    audio.play();
+    currentlyPlaying = true;
+  };
+
+  returnObj.pause = function() {
+    audio.pause()
+    currentlyPlaying = false;
+  };
+
+  returnObj.resume = function() {
+    audio.play();
+    currentlyPlaying = true;
+  };
+
+  returnObj.isPlaying = function() {
+    return currentlyPlaying;
+  };
+
+  returnObj.getCurrentSong = function() {
+
+  };
+
+  returnObj.next = function() {
+
+  };
+
+  returnObj.previous = function() {
+
+  };
+
+  returnObj.getProgress = function() {
+
+  };
+
+
+
+
+  return returnObj;
 });
